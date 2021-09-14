21a2ce5822beccdf819f2a9845222450f6aba53e
Kenan Warren <kenan.warren@jupiterone.com>
2021-09-14 15:28:01 -0400

    Base vpc name off cluster name instead of using tf edu boilerplate

--- terraform/eks/vpc.tf
+++ terraform/eks/vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
 
-  name                 = "education-vpc"
+  name                 = "${local.cluster_name}-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
