f63c99af7eaf288a890df7de68312a9c04c69ba7
eXtremeX <viragsf@gmail.com>
2022-05-16 16:04:00 +0200

    Add IPs related data to kube_node

--- src/steps/nodes/converters.test.ts
+++ src/steps/nodes/converters.test.ts
@@ -5,4 +5,44 @@ describe('#createNodeEntity', () => {
   test('should convert data', () => {
     expect(createNodeEntity(createMockNode())).toMatchSnapshot();
   });
+
+  test('should parse status.addresses correctly', () => {
+    expect(
+      createNodeEntity({
+        ...createMockNode(),
+        status: {
+          addresses: [
+            {
+              address: '1.1.1.1',
+              type: 'InternalIP',
+            },
+            {
+              address: '2.2.2.2',
+              type: 'InternalIP',
+            },
+            {
+              address: '5.5.5.5',
+              type: 'ExternalIP',
+            },
+            {
+              address: '6.6.6.6',
+              type: 'ExternalIP',
+            },
+            {
+              address: 'minikube',
+              type: 'Hostname',
+            },
+            {
+              address: 'internalDns',
+              type: 'InternalDns',
+            },
+            {
+              address: 'externalDns',
+              type: 'ExternalDns',
+            },
+          ],
+        },
+      }),
+    ).toMatchSnapshot();
+  });
 });
