f3301d956b94b9e9a8c5f3d7746748f6d2994a52
Paul Ilaga <pgilaga@gmail.com>
2022-10-15 23:07:09 +0800

    Add additional cluster entity fields

--- src/steps/clusters/converters.ts
+++ src/steps/clusters/converters.ts
@@ -1,22 +1,24 @@
 import { createIntegrationEntity } from '@jupiterone/integration-sdk-core';
+import { Cluster } from '@kubernetes/client-node';
 import { Entities } from '../constants';
 
 function getClusterKey(id: string) {
   return `kube_cluster:${id}`;
 }
 
-export function createClusterEntity(name: string, id: string) {
+export function createClusterEntity(data: Cluster) {
   return createIntegrationEntity({
     entityData: {
-      source: {
-        name,
-      },
+      source: data,
       assign: {
         _class: Entities.CLUSTER._class,
         _type: Entities.CLUSTER._type,
-        _key: getClusterKey(id),
-        name: name,
-        displayName: name,
+        _key: getClusterKey(data.name),
+        name: data.name,
+        displayName: data.name,
+        server: data.server,
+        skipTlsVerify: data.skipTLSVerify,
+        caData: data.caData,
       },
     },
   });
