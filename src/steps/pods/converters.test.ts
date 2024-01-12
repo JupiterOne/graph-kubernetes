5056bc40d72015d0c3fc60be209006dfa92cc932
Matthew Zember <matthew.zember@jupiterone.com>
2024-01-12 13:09:17 -0500

    remove unused steps

--- src/steps/pods/converters.test.ts
+++
@@ -1,8 +0,0 @@
-import { createPodEntity } from './converters';
-import { createMockPod } from '../../../test/mocks';
-
-describe('#createPodEntity', () => {
-  test('should convert data', () => {
-    expect(createPodEntity(createMockPod())).toMatchSnapshot();
-  });
-});
