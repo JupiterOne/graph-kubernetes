9b4960051c91819c9be1cc988e653e2d4ac7e225
Gonzalo Avalos Ribas <gonzalo.avalos.ribas@contractor.jupiterone.com>
2024-06-27 11:15:09 -0300

    Husky config update

--- husky.config.js
+++ husky.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   hooks: {
-    'pre-commit': `npx j1-integration document && git add docs/jupiterone.md && lint-staged && npm run document:permissions && npx prettier --write docs/jupiterone.md && git add docs/jupiterone.md`,
+    'pre-commit': `npx j1-integration document && git add docs/jupiterone.md && lint-staged && npx prettier --write docs/jupiterone.md && git add docs/jupiterone.md`,
     'pre-push': 'npm run prepush',
   },
 };
