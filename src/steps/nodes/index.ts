e15249d93dc64b3de427b40d1d88940da3204987
Gonzalo Avalos Ribas <gonzalo.avalos.ribas@contractor.jupiterone.com>
2024-08-13 11:12:29 -0300

    Prevent nodes with missing metadata to be created

--- src/steps/nodes/index.ts
+++ src/steps/nodes/index.ts
@@ -1,22 +1,36 @@
-import { IntegrationStep } from '@jupiterone/integration-sdk-core';
+import {
+  IntegrationStep,
+  IntegrationWarnEventName,
+} from '@jupiterone/integration-sdk-core';
 import { IntegrationConfig, IntegrationStepContext } from '../../config';
 import { Entities, IntegrationSteps } from '../constants';
 import { createNodeEntity } from './converters';
 import { cacheNodeNameToUid } from '../../util/jobState';
 import getOrCreateAPIClient from '../../kubernetes/getOrCreateAPIClient';
+import { V1Node } from '@kubernetes/client-node';
 
 export async function fetchNodes(
   context: IntegrationStepContext,
 ): Promise<void> {
-  const { instance, jobState } = context;
+  const { instance, jobState, logger } = context;
   const { config } = instance;
+  let nodesWithMissingMetadata = 0;
 
   const client = getOrCreateAPIClient(config);
 
-  await client.iterateNodes(async (node) => {
+  await client.iterateNodes(async (node: V1Node) => {
+    if (!node.metadata) {
+      nodesWithMissingMetadata++;
+      return;
+    }
     const nodeEntity = createNodeEntity(node);
     await jobState.addEntity(nodeEntity);
     await cacheNodeNameToUid(jobState, node);
+  });
+
+  logger.publishWarnEvent({
+    name: IntegrationWarnEventName.IngestionLimitEncountered,
+    description: `Found metadata missing for ${nodesWithMissingMetadata} nodes`,
   });
 }
 
